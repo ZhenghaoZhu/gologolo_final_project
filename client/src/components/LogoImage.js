cf5ef17c0c9b9ed0461980fb45b645ff13faae35
Zhenghao Zhu <kevinzhuzhenghao@gmail.com>
2020-05-11 00:47:12 -0400

    Updated divs for image and text box components.

--- client/src/components/LogoImage.js
+++ client/src/components/LogoImage.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
-import { Grid, Button, TextField } from '@material-ui/core';
-import { Resizable, ResizableBox } from 'react-resizable';
-import Draggable from 'react-draggable';
+import { Rnd } from 'react-rnd';
 
 // THIS IS HOW WE DISPLAY THE LOGO, IN THIS COMPONENT
 class LogoImage extends Component {
 
-    render() {
-        const styles = {
-            container: {
-                
-            }
-        }
+    constructor(props) {
+        super(props)
+    }
 
+    render() {
         return (
-            // <div>
-                    <img src = "https://rlv.zcache.com/svc/view?rlvnet=1&realview=113297416891423264&design=b5c64d95-6c7b-48e6-8551-51d507499df1&size=1.5&style=square_sticker&max_dim=60"></img>
-            // </div>
+            <Rnd
+                style = {{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    border: "solid 1px #ddd",
+                    background: "#f0f0f0"
+                  }}
+                default = {{
+                    x : this.props.style.xCoord,
+                    y : this.props.style.yCoord,
+                    width : 200,
+                    height : 200
+                }}
+            >
+                <img src = {this.props.style.source} />
+            </Rnd>
         )
     }
+    
 }
 
 export default LogoImage
--- client/src/components/LogoTextBox.js
+++ client/src/components/LogoTextBox.js
@@ -1,17 +1,43 @@
 import React, { Component } from 'react';
+import { Rnd } from 'react-rnd';
 
 // THIS IS HOW WE DISPLAY THE LOGO, IN THIS COMPONENT
 class LogoTextBox extends Component {
+    constructor(props) {
+        super(props);
 
+        const rndStyle = {
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            border: "solid 1px #ddd",
+            background: "#f0f0f0"
+          };
+    }
     render() {
         return (
-            <div className="displayedLogo" id = "logoTextDiv"
-                style={ this.props.style }
-                contentEditable='true'
-                h = { parseInt(this.props.h) }
-                w = { parseInt(this.props.w) }>
-                {this.props.text}
-            </div>
+            <Rnd
+                style = {{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    border: "solid 1px #ddd",
+                    background: "#f0f0f0"
+                  }}
+                default = {{
+                    x : this.props.style.xCoord,
+                    y : this.props.style.yCoord,
+                    width : 200,
+                    height : 200
+                }}
+            >
+                <div className="displayedLogo" id = "logoTextDiv"
+                    style={ this.props.style }
+                    contentEditable='true'
+                    >
+                    {this.props.style.text}
+                </div>
+            </Rnd>
         )
     }
 }
